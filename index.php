6d1df494dc2d65aae247dc586000791ae91e4c2f
ParraChan <37112218+ParraChan@users.noreply.github.com>
2020-10-31 00:04:14 -0600

    video 4-5 clases

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php declare(strict_types=1); 
 
-function getPermissionsByRole(string $role) :Array{
-	$permissions = [
-		'admin' => [
-				'Puede crear usuarios',
-				'Puede actualizar usuarios',
-				'Puede eliminar usuarios',
-				'Puede visualizar usuarios'
-		],
-		'moderator' => [
-				'Puede actualizar usuarios',
-				'Puede visualizar usuarios'
-		]
-	];
-	return $permissions[$role] ?? [
-			'Puede visualizar usuarios'
-	]; 
- }
+class mobileDevice
+{	
+	public $brand;
+	public $model;
+	public $color;
 
- var_dump(getPermissionsByRole('admin'));
- print "\n";
-  var_dump(getPermissionsByRole('moderator'));
-  print "\n";
-  var_dump(getPermissionsByRole('guest'));
- ?>
+	public function turnOn()
+	{
+		echo "$this->brand ha sido encendido";
+	}
+
+	public function makeACall(string $phoneNumber)
+	{
+		echo "$this->brand esta llamando al numero: $phoneNumber";
+	}
+}
+	$obj = new mobileDevice();
+	$obj->brand ='Iphone';
+	$obj->model ='8+';
+	$obj->color = 'White';
+
+	var_dump($obj);
+	$obj->turnOn();
+	print "\n";
+	$obj->makeACall('2345');
