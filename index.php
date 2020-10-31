3e433b9f1fe52a3069c83b8ce5a9f08fedab6f58
ParraChan <37112218+ParraChan@users.noreply.github.com>
2020-10-30 20:48:56 -0600

    video 2

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php declare(strict_types=1); 
 
-function saySomething(string $string){
-	echo $string;
+function saySomething(string $string): string{
+	return 'first ' . $string;
 }
 
-function howOldAreYou(int $year){
-	print ((int) date('Y'))-$year;
+function howOldAreYou(int $year): int{
+	return ((int) date('Y'))-$year;
+}
+echo saySomething("slap\n");
+echo howOldAreYou(1995);
+print "\n";
+
+function discount(float $price, float &$discount): void{
+	$discount = $price * 0.2;
 }
 
-//howOldAreYou(1995);
-//saySomething("slap\n");
+$price = 1000;
+$discount = 0;
+
+discount($price,$discount);
+print sprintf('Precio inicial: %s',$price);
+print "\n";
+print sprintf('Descuento: %s',$discount);
+print "\n";
+print sprintf('Precio final: %s',$price-$discount);
+
 //saySomething("hey ya");
-print date('Y-M-D');
+//print date('Y-M-D');
 //saySomething(200.00);
  ?>
