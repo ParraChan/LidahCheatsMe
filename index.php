263f2eb61791526861d10fb26725a92fdba8b478
ParraChan <37112218+ParraChan@users.noreply.github.com>
2020-11-02 20:02:20 -0600

    Video 7 modificadores publicos y privados

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <?php declare(strict_types=1); 
 
-class mobileDevice
-{	
+class Product
+{
 
-	public function __construct(string $brand, string $model, string $color){
-		$this->brand=$brand;
-		$this->model=$model;
-		$this->color=$color;
+	private $name;
+	private $price;
 
+	public function getName():string{
+		return $this->name;
 	}
 
-	public $brand;
-	public $model;
-	public $color;
-
-	public function turnOn():string 
-	{
-		return "$this->brand ha sido encendido";
+	public function setPrice(float $price):void{
+		$this->price=$price;
+	}
+	public function getPrice():float{
+		return $this->price;
+	}
+	public function getDiscount():float{
+		return $this->calculateDiscount();
 	}
 
-	public function makeACall(string $phoneNumber):string
+	private function calculateDiscount():float{
+	$discount=0.2;
+	return (1-$discount)*$this->price;	
+	}
+
+	public function __construct(string $name, float $price)
 	{
-		return "$this->brand esta llamando al numero: $phoneNumber";
+		$this->name=$name;
+		$this->price=$price;
 	}
 }
-
-	$obj1 = new mobileDevice('Iphone','X','White');
-	$obj2 = new mobileDevice('Iphone','XII','White');
-	var_dump($obj1);
-	var_dump($obj2);
 	
+$obj = new Product('Guitarra electrica',1500);
+//$obj->name = 'Amplificador';
+var_dump($obj->getName());
+$obj->setPrice(1900);
+var_dump($obj->getPrice());
+var_dump($obj->getDiscount());
